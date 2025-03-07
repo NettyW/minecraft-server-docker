fea2ae1ce45b03d7a81e43563458df3f2d84687f
NettyW <omicron346@gmail.com>
2025-03-07 19:53:23 +0300

    Add rcon for serv. Refactor compose and dockerfile.

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /data/config /data/world
-chown -R ${USER_UID}:${USER_GID} /data/config /data/world
-chmod -R 755 /data/config /data/world
-exec "$@"
--- prepareenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USER_UID=$(id -u)
-USER_GID=$(id -g)
-
-echo "USER_UID=${USER_UID}" > .env
-echo "USER_GID=${USER_GID}" >> .env
-
-echo ".env файл успешно создан с USER_UID=${USER_UID} и USER_GID=${USER_GID}"
